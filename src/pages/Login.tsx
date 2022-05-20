8671cb0eb38609e8b1ee202fff7ddd2379da81f8
Cedrik Nguefack <cedric.nguefack@tapmeppe.com>
2022-05-21 02:40:26 +0300

    initialisation de l'application web

---
+++ src/pages/Login.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './logo.svg';
+import '../styles/login.scss';
+
+export default class Login extends React.Component {
+
+	constructor(props: any){
+		super(props)
+	}
+
+	render: () => React.ReactNode = () => {
+		return <div className='login'>
+		</div>
+	}
+}
